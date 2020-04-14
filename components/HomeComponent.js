031d7bf264c6736ba822f19502dedc7d8409f2ff
Oliver Farren <oliver.farren@gmail.com>
2020-04-14 16:36:11 +0100

    Navigation Part 2

---
+++ components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text, Platform } from 'react-native';
+
+class Home extends Component {
+
+    render() {
+        return(
+            <View><Text>Home Components</Text></View>
+        );
+    }
+}
+
+export default Home;
