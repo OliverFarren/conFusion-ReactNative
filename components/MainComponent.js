c5fb5d2bbba6bfb41cbeb4e94cbb64b273793510
Oliver Farren <oliver.farren@gmail.com>
2020-04-15 18:28:32 +0100

    W1 Assignment

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { Button, View, Platform } from 'react-native';
 import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import Menu from './MenuComponent';
 import Home from './HomeComponent';
+import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+
+/*
+Notes to Coursera Reviewers
+
+Due to difficulties getting this course working as described, this project 
+uses the latest version of react-navigation which has meant
+the construct of the Stack Navigator and Drawer Navigator are different.
+
+I have written about this in the forum:
+https://www.coursera.org/learn/react-native/discussions/weeks/1/threads/8PifLG4EQ724nyxuBDO9DQ
+
+Where you can see the details of the Stack Navigator version differences.
+
+Please don't penalise my solution for not being an exact copy since it still implements
+the Week 1 assignment functionality (to the best of my knowledge) correctly.
+
+Also, if you read my solution on the forum, and find it useful, please upvote so
+that others can find it.
+*/
+
 
 const MenuNavigator = createStackNavigator();
 
@@ -45,7 +66,7 @@ const HomeNavigator = createStackNavigator();
 function HomeNavigatorScreen() {
     return(
         <HomeNavigator.Navigator
-            initialRouteName='Menu'
+            initialRouteName='Home'
             screenOptions={HeaderOptions}
         >
             <HomeNavigator.Screen
@@ -54,6 +75,39 @@ function HomeNavigatorScreen() {
             />
         </HomeNavigator.Navigator>
     );
+}
+
+const ContactNavigator = createStackNavigator();
+
+function ContactNavigatorScreen(){
+    return(
+        <ContactNavigator.Navigator
+            initialRouteName='Contact Us'
+            screenOptions={HeaderOptions}
+        >
+            <ContactNavigator.Screen
+                name="Contact Us"
+                component={Contact}
+            />
+        </ContactNavigator.Navigator>
+    );
+}
+
+const AboutUsNavigator = createStackNavigator();
+
+function AboutUsNavigatorScreen(){
+    return(
+        <AboutUsNavigator.Navigator
+            initialRouteName='About Us'
+            screenOptions={HeaderOptions}
+        >
+            <ContactNavigator.Screen
+                name="About Us"
+                component={About}
+            />
+        </AboutUsNavigator.Navigator>
+
+    )
 }
 
 const MainNavigator = createDrawerNavigator();
@@ -66,8 +120,10 @@ function MainNavigatorDrawer() {
                 backgroundColor:'#D1C4E9'
             }}
         >
-            <MainNavigator.Screen name="Home" component={HomeNavigatorScreen} />
-            <MainNavigator.Screen name="Menu" component={MenuNavigatorScreen} />
+            <MainNavigator.Screen name="Home"       component={HomeNavigatorScreen} />
+            <MainNavigator.Screen name="Contact Us" component={ContactNavigatorScreen} />
+            <MainNavigator.Screen name="Menu"       component={MenuNavigatorScreen} />
+            <MainNavigator.Screen name="About Us"   component={AboutUsNavigatorScreen} />
         </MainNavigator.Navigator>
     );
 }
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
+
+
 
 class Menu extends Component {
 
@@ -38,6 +40,8 @@ class Menu extends Component {
             />
         );
     }
+
+
 }
 
 
