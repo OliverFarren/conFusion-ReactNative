a26d4ff6b99c655eee7991849d5ef4e994d871f5
Oliver Farren <oliver.farren@gmail.com>
2020-04-20 22:48:01 +0100

    Activity Indicators

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, View, FlatList } from 'react-native';
 import { ListItem, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -36,20 +37,43 @@ class About extends Component {
             );
         }
 
-        return(
-            <ScrollView>
-                <History/>
-                <Card
-                    title="Corporate Leadership"
-                >
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
                     <FlatList 
                         data={this.props.leaders.leaders}
                         renderItem={renderLeader}
                         keyExtractor={item => item.id.toString()}
                         />
-                </Card>
-            </ScrollView>
-        );
+                    </Card>
+                </ScrollView>
+            );
+        }
     }
 }
 
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, FlatList, StyleSheet } from 'react-native';
 import { Tile } from 'react-native-elements';
+import { Loading } from './LoadingComponent';
 
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -31,13 +32,27 @@ class Menu extends Component {
             );
         };
 
-        return (
-            <FlatList 
-                data={this.props.dishes.dishes}
-                renderItem={renderMenuItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        );
+        if (this.props.dishes.isLoading) {
+            return(
+                <Loading />
+            );
+        }
+        else if (this.props.dishes.errMess) {
+            return(
+                <View>            
+                    <Text>{props.dishes.errMess}</Text>
+                </View>            
+            );
+        }
+        else {
+            return (
+                <FlatList 
+                    data={this.props.dishes.dishes}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}
+                    />
+            );
+        }
     }
 
 
